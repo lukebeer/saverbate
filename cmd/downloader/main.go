ef3fca9bcfa7d53b88b70eed0713fb79086a2b47
Andrew N. Shalaev <isqad88@gmail.com>
2020-10-25 18:23:25 +0500

    feature: development mailer service

--- cmd/downloader/main.go
+++ cmd/downloader/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"time"
+
+	"github.com/nats-io/nats.go"
+	flag "github.com/spf13/pflag"
+	"github.com/spf13/viper"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	flag.String("natsAddress", "nats://localhost:10222", "Address to connect to NATS server")
+	flag.Parse()
+
+	viper.BindPFlags(flag.CommandLine)
+
+	// NATS
+	nc, err := nats.Connect(viper.GetString("natsAddress"), nats.NoEcho())
+	if err != nil {
+		log.Panic(err)
+	}
+	defer nc.Close()
+
+	// Subscribe
+	if _, err := nc.QueueSubscribe("downloading", "download", func(m *nats.Msg) {
+		// Use the response
+		log.Printf("Reply: %s", m.Data)
+
+		// some long task
+		time.Sleep(5 * time.Second)
+	}); err != nil {
+		log.Fatal(err)
+	}
+
+	select {}
 }
--- cmd/mailer/main.go
+++ cmd/mailer/main.go
@@ -1,7 +1,64 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/emersion/go-imap/client"
+	"github.com/gocraft/work"
+	"github.com/gomodule/redigo/redis"
+)
+
+// Make a redis pool
+var redisPool = &redis.Pool{
+	MaxActive: 5,
+	MaxIdle:   5,
+	Wait:      true,
+	Dial: func() (redis.Conn, error) {
+		return redis.Dial("tcp", "saverbate-redis:6379")
+	},
+}
+
+type Context struct{}
 
 func main() {
-	fmt.Println("Hello, world!")
+	pool := work.NewWorkerPool(Context{}, 10, "saverbate_downloads", redisPool)
+	pool.PeriodicallyEnqueue("0 */5 * * * *", "check_email")
+
+	pool.Job("check_email", (*Context).CheckEmail)
+
+	// Start processing jobs
+	pool.Start()
+
+	// Wait for a signal to quit:
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt, os.Kill)
+	<-signalChan
+
+	// Stop the pool
+	pool.Stop()
+}
+
+func (ctx *Context) CheckEmail(job *work.Job) error {
+	log.Println("Connecting to server...")
+
+	// Connect to server
+	c, err := client.DialTLS("imap.gmail.com:993", nil)
+	if err != nil {
+		log.Printf("Error: %v", err)
+		return err
+	}
+
+	// Don't forget to logout
+	defer c.Logout()
+
+	// Login
+	if err := c.Login("saverbate@gmail.com", "sUpervised896491"); err != nil {
+		log.Printf("Error: %v", err)
+		return err
+	}
+	log.Println("Logged in")
+
+	return nil
 }
