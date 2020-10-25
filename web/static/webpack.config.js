61ec90498ec5225901a976a61f18fc034f84394f
Andrew N. Shalaev <isqad88@gmail.com>
2020-10-25 16:31:17 +0500

    feature: homepage skeleton

---
+++ web/static/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+module.exports = {
+  plugins: [new MiniCssExtractPlugin()],
+  entry: {
+    main: './src/main.js',
+  },
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: '[name].bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      }
+    ]
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin(),
+    ],
+  },
+};
