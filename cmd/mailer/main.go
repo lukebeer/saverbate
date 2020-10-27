799ee6dfc99961c7712b82a2307f45420cf1fc97
Andrew N. Shalaev <isqad88@gmail.com>
2020-10-27 16:08:26 +0500

    feature: read mail and retreiving performer names

--- cmd/mailer/main.go
+++ cmd/mailer/main.go
@@ -1,32 +1,42 @@
 package main
 
 import (
-	"log"
 	"os"
 	"os/signal"
 
-	"github.com/emersion/go-imap/client"
+	"saverbate/pkg/mailer"
+
 	"github.com/gocraft/work"
 	"github.com/gomodule/redigo/redis"
+	flag "github.com/spf13/pflag"
+	"github.com/spf13/viper"
 )
 
-// Make a redis pool
-var redisPool = &redis.Pool{
-	MaxActive: 5,
-	MaxIdle:   5,
-	Wait:      true,
-	Dial: func() (redis.Conn, error) {
-		return redis.Dial("tcp", "saverbate-redis:6379")
-	},
-}
-
-type Context struct{}
+const concurrency = 1
 
 func main() {
-	pool := work.NewWorkerPool(Context{}, 10, "saverbate_downloads", redisPool)
-	pool.PeriodicallyEnqueue("0 */5 * * * *", "check_email")
+	flag.String("user", "saverbate@gmail.com", "Username for imap server")
+	flag.String("password", "sUpervised896491", "Password for imap server")
+	flag.String("redisAddress", "saverbate-redis:6379", "Address to redis server")
+	flag.String("natsAddress", "nats://saverbate-nats:4222", "Address to connect to NATS server")
+	flag.Parse()
 
-	pool.Job("check_email", (*Context).CheckEmail)
+	viper.BindPFlags(flag.CommandLine)
+
+	// Make a redis pool
+	redisPool := &redis.Pool{
+		MaxActive: 5,
+		MaxIdle:   5,
+		Wait:      true,
+		Dial: func() (redis.Conn, error) {
+			return redis.Dial("tcp", viper.GetString("redisAddress"))
+		},
+	}
+
+	pool := work.NewWorkerPool(mailer.Context{}, concurrency, mailer.NamespaceDownloads, redisPool)
+	pool.PeriodicallyEnqueue("0 */15 * * * *", mailer.JobName)
+
+	pool.Job(mailer.JobName, (*mailer.Context).CheckEmail)
 
 	// Start processing jobs
 	pool.Start()
@@ -38,27 +48,4 @@ func main() {
 
 	// Stop the pool
 	pool.Stop()
-}
-
-func (ctx *Context) CheckEmail(job *work.Job) error {
-	log.Println("Connecting to server...")
-
-	// Connect to server
-	c, err := client.DialTLS("imap.gmail.com:993", nil)
-	if err != nil {
-		log.Printf("Error: %v", err)
-		return err
-	}
-
-	// Don't forget to logout
-	defer c.Logout()
-
-	// Login
-	if err := c.Login("saverbate@gmail.com", "sUpervised896491"); err != nil {
-		log.Printf("Error: %v", err)
-		return err
-	}
-	log.Println("Logged in")
-
-	return nil
 }
---
+++ pkg/mailer/context.go
@@ -0,0 +1,167 @@
+package mailer
+
+import (
+	"io"
+	"io/ioutil"
+	"log"
+	"regexp"
+
+	"github.com/emersion/go-imap"
+	"github.com/emersion/go-imap/client"
+	"github.com/emersion/go-message/mail"
+	"github.com/gocraft/work"
+	"github.com/spf13/viper"
+)
+
+const (
+	imapServer = "imap.gmail.com:993"
+	// NamespaceDownloads is redis namespace
+	NamespaceDownloads = "saverbate_downloads"
+	// JobName is job name for periodically enqueuing
+	JobName = "check_email"
+)
+
+// Context is context of mailer service
+type Context struct{}
+
+var (
+	performersLinkRe = regexp.MustCompile(`(https://chaturbate\.com\/[^\/]{3,}/)(?:\n|\]|<||\s|$)`)
+	performerNameRe  = regexp.MustCompile(`https://chaturbate\.com\/([^\/]{3,})\/`)
+)
+
+// CheckEmail connects to imap server and check new emails
+func (ctx *Context) CheckEmail(job *work.Job) error {
+	performersUnique := make(map[string]struct{})
+	performers := []string{}
+
+	log.Println("Connecting to server...")
+
+	// Connect to server
+	c, err := client.DialTLS(imapServer, nil)
+	if err != nil {
+		log.Printf("Error: %v", err)
+		return err
+	}
+
+	// Don't forget to logout
+	defer c.Logout()
+
+	// Login
+	if err := c.Login(viper.GetString("user"), viper.GetString("password")); err != nil {
+		log.Printf("Error: %v", err)
+		return err
+	}
+	log.Println("Logged in")
+
+	// Select INBOX
+	mbox, err := c.Select("INBOX", false)
+	if err != nil {
+		log.Printf("Error: %v", err)
+		return err
+	}
+	log.Println("Flags for INBOX:", mbox.Flags)
+
+	if mbox.Messages == 0 {
+		log.Printf("Empty mailbox...Exit")
+		return nil
+	}
+	from := uint32(1)
+	to := mbox.Messages
+	if mbox.Messages > 49 {
+		// We're using unsigned integers here, only substract if the result is > 0
+		from = mbox.Messages - 49
+	}
+	seqset := new(imap.SeqSet)
+	seqset.AddRange(from, to)
+
+	var section imap.BodySectionName
+	items := []imap.FetchItem{section.FetchItem()}
+
+	messages := make(chan *imap.Message, 10)
+	done := make(chan error, 1)
+	go func() {
+		done <- c.Fetch(seqset, items, messages)
+	}()
+
+	if messages == nil {
+		log.Println("Server didn't returned message")
+		return nil
+	}
+
+	for msg := range messages {
+		for s := range msg.Body {
+			r := msg.GetBody(s)
+			if r == nil {
+				log.Println("No body for given section")
+				break
+			}
+
+			mr, err := mail.CreateReader(r)
+			if err != nil {
+				log.Printf("Error: %v", err)
+				break
+			}
+
+			header := mr.Header
+			if subject, err := header.Subject(); err == nil {
+				matched, _ := regexp.MatchString(
+					`(?:someone you follow is chaturbating|broadcasters you follow are chaturbating)$`,
+					subject,
+				)
+				if !matched {
+					break
+				}
+			}
+
+			// Process each message's part
+			for {
+				p, err := mr.NextPart()
+				if err == io.EOF {
+					break
+				} else if err != nil {
+					log.Printf("Error: %v", err)
+					return nil
+				}
+
+				switch p.Header.(type) {
+				case *mail.InlineHeader:
+					// This is the message's text (can be plain-text or HTML)
+					b, _ := ioutil.ReadAll(p.Body)
+					matches := performersLinkRe.FindAllStringSubmatch(string(b), -1)
+					if len(matches) == 0 {
+						break
+					}
+
+					for _, matching := range matches {
+						if _, ok := performersUnique[matching[1]]; !ok {
+							performersUnique[matching[1]] = struct{}{}
+						}
+					}
+				case *mail.AttachmentHeader:
+					// This is an attachment
+					// filename, _ := h.Filename()
+					// log.Printf("Got attachment: %v", filename)
+				}
+			}
+		}
+	}
+
+	if err := <-done; err != nil {
+		log.Printf("Error: %v", err)
+		return err
+	}
+
+	log.Println("Done!")
+
+	for performer := range performersUnique {
+		name := performerNameRe.FindStringSubmatch(performer)
+		if len(name) == 0 {
+			continue
+		}
+		performers = append(performers, name[1])
+	}
+
+	log.Println("Performers: ", performers)
+
+	return nil
+}
