eecdccda24ea3fc5f9b378d575538b3a9fcfc51e
Andrew N. Shalaev <isqad88@gmail.com>
2020-11-05 16:45:13 +0500

    feature: add portal user credentials to farm

--- pkg/farm/farm.go
+++ pkg/farm/farm.go
@@ -21,14 +21,17 @@ const (
 
 // Instance is instance of the farm
 type Instance struct {
-	ID       int64  `db:"id"`
-	Name     string `db:"name"`
-	State    State  `db:"state"`
-	Password string `db:"password"`
+	ID              int64  `db:"id"`
+	Name            string `db:"name"`
+	Password        string `db:"password"`
+	MailerState     State  `db:"mailer_state"`
+	SubscriberState State  `db:"subscriber_state"`
+	PortalUsername  string `db:"portal_username"`
+	PortalPassword  string `db:"portal_password"`
 }
 
 // FindFree finds free (inactive) farm for occupation
-func FindFree(db *sqlx.DB) (*Instance, error) {
+func FindFree(db *sqlx.DB, serviceName string) (*Instance, error) {
 	farm := &Instance{}
 
 	tx, err := db.Begin()
@@ -36,13 +39,31 @@ func FindFree(db *sqlx.DB) (*Instance, error) {
 		return nil, err
 	}
 
-	row := tx.QueryRow(`SELECT id, name, state, password FROM farms WHERE state = 'inactive' LIMIT 1 FOR UPDATE`)
+	row := tx.QueryRow(`
+		SELECT
+			id,
+			name,
+			mailer_state,
+			subscriber_state,
+			password,
+			portal_username,
+			portal_password
+		FROM farms WHERE ` + serviceName + `_state = 'inactive' LIMIT 1 FOR UPDATE
+	`)
 	if err != nil {
 		tx.Rollback()
 		return nil, err
 	}
 
-	err = row.Scan(&(farm.ID), &(farm.Name), &(farm.State), &(farm.Password))
+	err = row.Scan(
+		&(farm.ID),
+		&(farm.Name),
+		&(farm.MailerState),
+		&(farm.SubscriberState),
+		&(farm.Password),
+		&(farm.PortalUsername),
+		&(farm.PortalPassword),
+	)
 	if err == sql.ErrNoRows {
 		tx.Rollback()
 		return nil, errors.New("No free farms")
@@ -53,7 +74,7 @@ func FindFree(db *sqlx.DB) (*Instance, error) {
 		}
 	}
 
-	_, err = tx.Exec(`UPDATE farms SET state = 'active' WHERE id = $1`, farm.ID)
+	_, err = tx.Exec(`UPDATE farms SET `+serviceName+`_state = 'active' WHERE id = $1`, farm.ID)
 	if err != nil {
 		tx.Rollback()
 		return nil, err
@@ -61,14 +82,18 @@ func FindFree(db *sqlx.DB) (*Instance, error) {
 
 	tx.Commit()
 
-	farm.State = StateActive
+	if serviceName == "mailer" {
+		farm.MailerState = StateActive
+	} else {
+		farm.SubscriberState = StateActive
+	}
 
 	return farm, nil
 }
 
 // Release move farm to inactive
-func Release(db *sqlx.DB, name string) error {
-	_, err := db.Exec(`UPDATE farms SET state = 'inactive' WHERE name = $1`, name)
+func Release(db *sqlx.DB, serviceName, name string) error {
+	_, err := db.Exec(`UPDATE farms SET `+serviceName+`_state = 'inactive' WHERE name = $1`, name)
 	if err != nil {
 		return err
 	}
