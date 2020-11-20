b323571688b3c08c1967b20a11a53c62aeccbc7c
Andrew N. Shalaev <isqad88@gmail.com>
2020-11-20 16:07:38 +0500

    fix: add control to player

--- web/static/src/show.js
+++ web/static/src/show.js
@@ -1,21 +1,20 @@
 import Clappr from "@clappr/core";
 import HlsjsPlayback from "@clappr/hlsjs-playback";
+import { MediaControl, SpinnerThreeBounce } from "@clappr/plugins";
 
 document.addEventListener('DOMContentLoaded', function() {
   const player = new Clappr.Player({
     source: "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8",
-    plugins: [HlsjsPlayback],
+    plugins: [HlsjsPlayback, MediaControl, SpinnerThreeBounce],
     mute: true,
     parentId: "#player",
     poster: 'http://imgproxy.saverbate.localhost/rs/fit/800/0/sm/0/plain/local://static/jykfqy/a522691f-0402-4718-a180-a165db6bf0d8.jpg',
     playback: {
       hlsjsConfig: {
-        debug: true,
+        debug: false,
       }
     }
   });
 
-  console.log(player);
-
-  player.play();
+  player.core.mediaControl.enable();
 });
